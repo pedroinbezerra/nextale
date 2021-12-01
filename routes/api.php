96415a16aac7e46d5cbc44dfe4584da69965f379
pedroinbezerra <pedroigormt2214@gmail.com>
2021-12-01 07:42:22 -0300

    Route creation
    Route creation from file API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaleController;
+use App\Http\Controllers\FileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::resource('/tale', TaleController::class);
+Route::resource('/file', FileController::class);
