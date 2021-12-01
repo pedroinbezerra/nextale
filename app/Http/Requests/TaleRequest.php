39eaabde1c227393d0af9833a79c8c9fc9ad4d32
pedroinbezerra <pedroigormt2214@gmail.com>
2021-12-01 03:39:48 -0300

    Created controller, route and validator.

---
+++ app/Http/Requests/TaleRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class TaleRequest extends FormRequest
+{
+    // /**
+    //  * Determine if the user is authorized to make this request.
+    //  *
+    //  * @return bool
+    //  */
+    // public function authorize()
+    // {
+    //     return false;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|string',
+            'body' => 'required|string',
+            'is_enabled' => 'required|boolean',
+            'created_at' => 'required|date_format:Y-m-d H:i:s',
+            'updated_at' => 'required|date_format:Y-m-d H:i:s'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
